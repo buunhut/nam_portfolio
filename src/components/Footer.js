90b625298cd1d298d80ee66051da98694a3fca78
Buu Nhut <nhut.nta@gmail.com>
2025-07-24 14:53:51 +0700

    fisrt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const handleToTop = () => {
+    window.scrollTo({
+      top: 0,
+      // behavior: "smooth",
+    });
+  };
   return (
     <footer>
       <ul>
-        <li>
+        <li onClick={handleToTop}>
           <NavLink to={"/portfolio"}>Portfolio</NavLink>
         </li>
-        <li>
+        <li onClick={handleToTop}>
           <NavLink to={"/blog"}>Blog</NavLink>
         </li>
-        <li>
+        <li onClick={handleToTop}>
           <NavLink to={"/contact"}>Contact</NavLink>
         </li>
       </ul>
